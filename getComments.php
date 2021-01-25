2f55223faa38dc6ec13e2b71e031f5fc74aa851b
Julia Leblond <JuliaLblnd@users.noreply.github.com>
2021-01-25 13:00:59 +0100

    Refactoring and return full JSON if no bddtrans-id provided

--- getComments.php
+++ getComments.php
@@ -4,6 +4,7 @@ require __DIR__ . '/functions.php';
 $base_url = 'https://bddtrans.fr';
 $credentialsPath = __DIR__ . '/bddtrans_credentials.json';
 $commentsDbFile = __DIR__ . '/bddtrans_comments.json';
+$json_flags = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT;
 $time = time();
 
 $defaultOptions = [
@@ -34,37 +35,42 @@ if (file_exists($credentialsPath)) {
 	$bddtrans_credentials = json_decode(file_get_contents($credentialsPath), true);
 }
 if (empty($bddtrans_credentials['pseudo']) || empty($bddtrans_credentials['password'])) {
-	file_put_contents($credentialsPath, json_encode($bddtrans_credentials, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+	file_put_contents($credentialsPath, json_encode($bddtrans_credentials, $json_flags));
 	echo 'Remplire le fichier '.basename($credentialsPath).'.';
 	exit(1);
 }
 if (empty($bddtrans_credentials['token']) || $bddtrans_credentials['token_expires'] > $time) {
 	$login_url = $base_url.'/userpanel/connexion.php';
 	$bddtrans_credentials['token'] = getLoginToken($login_url, $bddtrans_credentials['pseudo'], $bddtrans_credentials['password']);
-	$bddtrans_credentials['token_expires'] = ($time + (60 * 60 * 48)); // Token expires in 48 hours
-	file_put_contents($credentialsPath, json_encode($bddtrans_credentials, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+	$bddtrans_credentials['token_expires'] = ($time + (60 * 60 * 10)); // Token expires in 10 hours
+	file_put_contents($credentialsPath, json_encode($bddtrans_credentials, $json_flags));
 }
 
 if (!file_exists($commentsDbFile)) {
-	file_put_contents($commentsDbFile, json_encode([], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+	file_put_contents($commentsDbFile, json_encode([], $json_flags));
 }
 
 $commentsDB = json_decode(file_get_contents($commentsDbFile), true);
 
-if (!isset($commentsDB[$bddtrans_id]) || $commentsDB[$bddtrans_id]['updated'] < ($time - (60 * 60 * 48)) || empty($commentsDB[$bddtrans_id]['comments'])) {
+if (
+	empty($commentsDB[$bddtrans_id])
+	OR $commentsDB[$bddtrans_id]['updated'] < ($time - (60 * 60 * 48))
+	OR empty($commentsDB[$bddtrans_id]['comments'])
+) {
 	$token = $bddtrans_credentials['token'];
 	$url = $base_url . '/' . $categorie . '/' . $bddtrans_id . '.html';
 
 	$commentsDB[$bddtrans_id]['comments'] = getComments($url, $token);
 	$commentsDB[$bddtrans_id]['updated'] = $time;
-	file_put_contents($commentsDbFile, json_encode($commentsDB, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+	file_put_contents($commentsDbFile, json_encode($commentsDB, $json_flags));
 }
 
 $comments = $commentsDB[$bddtrans_id]['comments'];
 
 if ($format == "json") {
+	$comments = empty($bddtrans_id) ? $commentsDB : $comments;
 	header('Content-Type: application/json');
-	echo json_encode(empty($comments) ? [] : $comments, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+	echo json_encode(empty($comments) ? [] : $comments, $json_flags);
 }
 else {
 	if (sizeof($comments) >= 1) {
